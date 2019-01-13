74471f5543e828192126f6eec519e311944ec4e2
vinodhmj <vinodhmjhuman@gmail.com>
2019-01-13 17:23:36 +0530

    config updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,3 +22,24 @@
 
 ;;(add-to-list 'load-path "~/.emacs.d/lisp")
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
+ '(package-selected-packages
+   (quote
+    (fountain-mode mw-thesaurus yasnippet-snippets which-key web-mode virtualenvwrapper use-package undo-tree tern-auto-complete synosaurus spacemacs-theme solarized-theme smex smartparens shell-switcher peep-dired paradox ox-reveal origami org-gcal org-bullets org-ac moe-theme magit irony-eldoc iedit hungry-delete htmlize hideshow-org ggtags flycheck-irony expand-region epresent engine-mode elpy elfeed-org elfeed-goodies edit-server dired-quick-sort dired+ diminish default-text-scale counsel-projectile company-irony-c-headers company-irony beacon base16-theme ace-window)))
+ '(paradox-github-token t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
