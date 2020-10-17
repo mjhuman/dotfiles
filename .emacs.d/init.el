1ffada3d5d160631008002520925b4fcdcb618f8
vinodhmj <mj@human>
2020-10-17 05:57:41 +0530

    init changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,48 +11,24 @@
 ;/                             ("org" . "https://orgmode.org/elpa/")))
 
 
-; initialize package.el
-
-(package-initialize)
-(package-refresh-contents)
+;; initialize package.el
 
 (require 'package)
-
 
 ;; add MELPA to repository list
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                            ("melpa" . "https://melpa.org/packages/")                        
                            ("org" . "https://orgmode.org/elpa/")))
 
+;; Initializes the package infrastructure
 (package-initialize)
+
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
 
 (unless (package-installed-p 'org-plus-contrib)
    (package-install 'org-plus-contrib))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/orgfiles/i.org" "~/Dropbox/orgfiles/arts.org" "~/Dropbox/orgfiles/books.org" "~/Dropbox/orgfiles/eee.org" "~/Dropbox/orgfiles/gcal.org" "~/Dropbox/orgfiles/inbox.org")))
- '(org-default-notes-file (concat org-directory "/notes.org"))
- '(org-directory "~/Dropbox/orgfiles")
- '(org-export-html-postamble nil)
- '(org-hide-leading-stars t)
- '(org-startup-folded (quote overview))
- '(org-startup-indented t)
- '(package-selected-packages
-   (quote
-    (true fountain org-latex openwith org-tempo gnu-elpa-keyring-update spinner yasnippet-snippets which-key web-mode virtualenvwrapper use-package undo-tree tern-auto-complete synosaurus spacemacs-theme solarized-theme smex smartparens shell-switcher peep-dired paradox ox-reveal ox-pandoc origami org-plus-contrib org-journal-list org-journal org-gcal org-bullets org-ac mw-thesaurus moe-theme magit-popup magit ivy-rtags irony-eldoc iedit hungry-delete htmlize hideshow-org graphql ghub ggtags fountain-mode flycheck-rtags flycheck-irony expand-region excorporate epresent engine-mode elpy elfeed-org elfeed-goodies edit-server dired-quick-sort dired+ diminish deft default-text-scale counsel-projectile company-irony-c-headers company-irony bm beacon base16-theme ag ace-window)))
- '(paradox-github-token t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 4096)) (:foreground "#c6c6c6" :background "#303030")) (((class color) (min-colors 256)) (:foreground "#c6c6c6" :background "#303030")) (((class color) (min-colors 89)) (:foreground "#c6c6c6" :background "#303030"))))
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
